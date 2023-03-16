becc2f871c9671404a4dbda5c45d154da7639a7a
cinnamondev <justpromptiguess@gmail.com>
2023-03-16 08:45:57 +0000

    Accept redirected I/O, README update.

--- fld2csv.py
+++ fld2csv.py
@@ -15,6 +15,8 @@
 
 # 15/03/2023: Moved to github repository.
 import os
+import sys
+import datetime
 import argparse 
 import re
 
@@ -25,13 +27,17 @@ https://github.com/cinnamondev/fld2csv.py/""",
                     formatter_class=argparse.RawTextHelpFormatter
                   )
 args.add_argument('file',
-                    type=argparse.FileType('r', encoding='UTF-8'),
-                    help="Location of file to parse.",
-                    )
+                     type=argparse.FileType('r', encoding='UTF-8'),
+                     default=sys.stdin,
+                     nargs="?",
+                     help="Location of file to parse. Also accepts piped input.",
+                     )                    
 args.add_argument('-o', '--output', 
                     type=argparse.FileType('w', encoding='UTF-8'),
-                    required=False,
-                    help="When specified, the output file will be saved to this location."
+                    default=sys.stdout,
+                    nargs="?",
+                    help="""When omitted, it will guess first on input filename, failing that, it will use <type>_<timestamp>.csv .
+If the output of the script is redirected, it will output results to stdout instead of a file."""
                     )
 args.add_argument('-t', '--type',
                      choices=["fld","elvis"],
@@ -44,16 +50,38 @@ args.add_argument('-n', '--quiet',
                      action='store_true',
                      required=False,
                      default=False,
-                     help="Execution with --quiet will not open the csv file in its associated program.")                     
+                     help="Execution with --quiet will not open the csv file in its associated program. Ignored if output is redirected.")
+"""args.add_argument('-v', '--verbose',
+                     action='store_true',
+                     required=False,
+                     default=False,
+                     help="Output debug messages.")"""                                          
 
 a = args.parse_args()
-if a.output == None:                       # handle no output file
-   fileName = a.file.name.rsplit('.',1)[0] + ".csv" # all cases
+
+# Input guard - only allow piped input or provided file. this looks ugly :(
+is_stdin = a.file.name == '<stdin>'
+is_stdout = a.output.name == '<stdout>'
+if not ((a.file.name == '<stdin>') and not sys.stdin.isatty()) or (a.file.name != '<stdin>'): 
+   print("""usage: fld2csv.py [-h] [-o OUTPUT] [-t {fld,elvis}] [-n] file
+fld2csv.py: error: the following arguments are required: file""")
+   exit()
+
+
+if a.output.name == '<stdout>' and sys.stdout.isatty():          # handle no output file
+   if a.file.name != '<stdin>':
+      fileName = a.file.name.rsplit('.',1)[0] + ".csv" # Guess using input file.
+   else:   
+      # Create file name using ISO timestamp.
+      filename = a.type +"_" + datetime.datetime.now().isoformat().replace(":","-") + ".csv"
+   
    a.output = open(
-      fileName,
+      filename,
       "w",
       encoding='UTF-8')
 
+print(a)
+# Now for the actual work!
 match a.type:
    case "fld":
       # Ignore line 0, 1 is already comma delim (ignore), 2 is space delim.
@@ -75,5 +103,5 @@ match a.type:
 a.output.close()
 a.file.close()  
 
-if not a.quiet:
+if not a.quiet and not is_stdout:
    os.startfile(a.output.name)
